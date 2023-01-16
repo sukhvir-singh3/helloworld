f10bc46909faac6da8a056c715cd63f845b4434e
Sukhvir Singh <sukhvir-singh3@myseneca.ca>
2023-01-16 12:14:33 -0500

    update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Sukhvir Singh - 155312218");
 });
 
 // setup http server to listen on HTTP_PORT
